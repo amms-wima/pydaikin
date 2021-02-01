0b3238a54e664bd663915018c198a21084e36ccf
Fredrik Erlandsson <fredrik.e@gmail.com>
2021-02-01 13:36:58 +0100

    refactor the factory

--- pydaikin/daikin_base.py
+++ pydaikin/daikin_base.py
@@ -47,37 +47,6 @@ class Appliance(DaikinPowerMixin):  # pylint: disable=too-many-public-methods
         return sorted(list(cls.TRANSLATIONS.get(dimension, {}).values()))
 
     @staticmethod
-    async def factory(device_id, session=None, **kwargs):
-        """Factory to init the corresponding Daikin class."""
-        from .daikin_airbase import (  # pylint: disable=import-outside-toplevel
-            DaikinAirBase,
-        )
-        from .daikin_brp069 import (  # pylint: disable=import-outside-toplevel
-            DaikinBRP069,
-        )
-        from .daikin_brp072c import (  # pylint: disable=import-outside-toplevel
-            DaikinBRP072C,
-        )
-        from .daikin_skyfi import DaikinSkyFi  # pylint: disable=import-outside-toplevel
-
-        if 'password' in kwargs and kwargs['password'] is not None:
-            appl = DaikinSkyFi(device_id, session, password=kwargs['password'])
-        elif 'key' in kwargs and kwargs['key'] is not None:
-            appl = DaikinBRP072C(
-                device_id,
-                session,
-                key=kwargs['key'],
-                uuid=kwargs.get('uuid'),
-            )
-        else:  # special case for BRP069 and AirBase
-            appl = DaikinBRP069(device_id, session)
-            await appl.update_status(appl.HTTP_RESOURCES[:1])
-            if appl.values == {}:
-                appl = DaikinAirBase(device_id, session)
-        await appl.init()
-        return appl
-
-    @staticmethod
     def parse_response(response_body):
         """Parse respose from Daikin."""
         response = dict([e.split('=') for e in response_body.split(',')])
